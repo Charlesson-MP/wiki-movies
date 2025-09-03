e58c6eaf4275940c60c159efe436fb793d82c84c
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2025-09-03 03:42:07 -0300

    feat: implementa funcionamento da requisição da api, criação dos cards dos filmes e coloca-os no container dos filmes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -33,3 +33,74 @@ themeToogle.addEventListener('change', () => {
     }
 });
 
+// request settings
+const API_KEY = '00a7e4e48d512ff2fd6a51f14146d5a7';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+let currentPage = 1;
+
+const moviesContainer = document.querySelector('.containerMovies');
+
+async function getMovies(page) {
+    const response = await fetch(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    
+    return response.json();
+}
+
+async function showMovies(page = 1) {
+    try {
+        const data = await getMovies(currentPage);
+
+        data.results.forEach(filme => {
+
+            const movieCard = document.createElement('div');
+            movieCard.classList.add('cardMovie');
+
+            const moviePoster = document.createElement('img');
+            moviePoster.setAttribute('src', `${BASE_IMG_URL}${filme.poster_path}`);
+            moviePoster.setAttribute('alt', 'Capa do filme');
+
+            const overlayMovieCard = document.createElement('div');
+            overlayMovieCard.classList.add('overlay');
+
+            const movieTitle = document.createElement('h2');
+            movieTitle.textContent = filme.title;
+
+            const btnDetais = document.createElement('button');
+            btnDetais.textContent = 'Detalhes';
+
+            overlayMovieCard.append(movieTitle, btnDetais);
+
+            movieCard.append(moviePoster, overlayMovieCard);
+
+            moviesContainer.appendChild(movieCard);
+        })
+
+    }catch(error) {
+        console.error('Tipo de erro: ', error);
+    }
+}
+
+showMovies();
+
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
+const numberPage = document.querySelector('.numberPage');
+
+btnPrev.addEventListener('click', () => {
+    [...moviesContainer.children].forEach(filme => filme.remove());
+    showMovies(--currentPage);
+
+    numberPage.textContent = currentPage;
+
+    if(currentPage === 1) btnPrev.disabled = true;
+});
+
+btnNext.addEventListener('click', () => {
+    if(btnPrev.disabled) btnPrev.disabled = false;
+
+    [...moviesContainer.children].forEach(filme => filme.remove());
+    showMovies(++currentPage);
+
+    numberPage.textContent = currentPage;
+});
