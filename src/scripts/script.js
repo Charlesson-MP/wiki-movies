ab9c84d5836c2164d416dbc431abd2f34b9ce840
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2025-09-03 01:23:45 -0300

    feat: implementa a interação do slider para troca de tema na página

---
+++ src/scripts/script.js
@@ -0,0 +1,34 @@
+// theme slider
+const themeToogle = document.querySelector('#themeToogle');
+
+// header elements
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const logoImg = document.querySelector('.logo img');
+const title = document.querySelector('h1');
+
+themeToogle.addEventListener('change', () => {
+    if(themeToogle.checked) {
+        // removing light theme
+        body.classList.remove('bodyLightTheme');
+        header.classList.remove('headerLightTheme');
+        title.classList.remove('titleLightTheme');
+
+        // adding dark theme
+        body.classList.add('bodyDarkTheme');
+        header.classList.add('headerDarkTheme');
+        logoImg.setAttribute('src', './src/images/logo-dark-theme.png')
+        title.classList.add('titleDarkTheme');
+    }else {
+        // removing dark theme
+        body.classList.remove('bodyDarkTheme');
+        header.classList.remove('headerDarkTheme');
+        title.classList.remove('titleDarkTheme');
+
+        // adding dark theme
+        body.classList.add('bodyLightTheme');
+        header.classList.add('headerLightTheme');
+        logoImg.setAttribute('src', './src/images/logo-light-theme.png')
+        title.classList.add('titleLightTheme');
+    }
+});
